0471de9b6a30ec4195c19477bcd9f2790741df64
Faris Al-khatahtbeh <48871391+farisalkhat@users.noreply.github.com>
2019-06-26 16:47:22 -0400

    Whoops. Removed tokens.py

--- tokens.py
+++
@@ -1,3 +0,0 @@
-discord_api = 'NTMzNDM4ODk1Njk5MDAxMzY0.XRKUAA.LeXu8RN7nTtC0TgPaSytxHj31cw'
-osu_api ='5e914b09d09d7ac4f37888f6e1fbd371e7c02066'
-google_api = 'AIzaSyDGQXcGGIyajK9P7XjQHt2yotAKoiAx1EM'
